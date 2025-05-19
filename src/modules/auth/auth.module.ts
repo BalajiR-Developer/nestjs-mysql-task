f1b74c22ee9eb5270d1661768d8c7cee921c3449
Balaji R <balaji.r@itechindia.co>
2025-05-19 15:24:39 +0530

    missed modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
   controllers: [AuthController],
   imports: [TypeOrmModule.forFeature([User])],
 })
--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat } from 'src/entities/chat.entity';
 
 @Module({
   providers: [ChatService],
-  controllers: [ChatController]
+  controllers: [ChatController],
+  imports: [TypeOrmModule.forFeature([Chat])],
 })
 export class ChatModule {}
